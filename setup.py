40e9a3bbcd7b4311595628ef8ce0b007d0c3ce28
Lev <vinnipyx38@gmail.com>
2023-08-22 13:24:00 +0500

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='extra_logging',
-      version='0.1.2',
+      version='0.1.3',
       description='Gaussian and Binomial distributions',
       packages=['extra_logging'],
       author_email='vinnipyx38@gmail.com',
